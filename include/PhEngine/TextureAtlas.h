b14899c07a0449dc683da7f34110ebb74ce4e355
silentptr <imquiet07@gmail.com>
2025-02-23 13:50:20 +1000

    fixed vector requirements for MSVC

--- include/PhEngine/TextureAtlas.h
+++ include/PhEngine/TextureAtlas.h
@@ -19,6 +19,9 @@ namespace PHENGINE_NAMESPACE
         inline AtlasPage(std::size_t w, std::size_t h, std::unique_ptr<unsigned char[]> d)
         : m_Width(w), m_Height(h), m_Data(std::move(d)) { }
 
+        // thanks MSVC
+        inline AtlasPage(AtlasPage&& page) : m_Width(page.m_Width), m_Height(page.m_Height), m_Data(std::move(page.m_Data)) { }
+
         inline unsigned char* GetData() { return m_Data.get(); }
     };
 
@@ -29,6 +32,8 @@ namespace PHENGINE_NAMESPACE
     public:
         inline TextureAtlas(std::vector<AtlasPage> p)
         : m_Pages(std::move(p)) { }
+
+        inline TextureAtlas(TextureAtlas&& atlas) : m_Pages(std::move(atlas.m_Pages)) { }
     };
 }
 
